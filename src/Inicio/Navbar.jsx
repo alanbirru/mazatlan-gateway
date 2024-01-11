b7b57d170cf9da16b1c198c81c9e8ccb32cd16a1
alanbirru <alanb_99@hotmail.com>
2024-01-11 12:18:30 -0700

    making the navbar

--- src/Inicio/Navbar.jsx
+++ src/Inicio/Navbar.jsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import "../css/navbar.css";
 import logo from "../assets/logo.png";
+import hamburger from "../assets/nav-icons/hamburger.png";
+import close from "../assets/nav-icons/close.png";
 
 export default function Navbar() {
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+  const [scrolling, setScrolling] = useState(false);
+
+  console.log(hamburgerOpen);
   const navItems = [
     { name: "Services" },
     { name: "Activities" },
@@ -13,19 +20,44 @@ export default function Navbar() {
     { name: "Eng-Es" },
   ];
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 500) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="nav-component">
+    <div className={`nav-component ${scrolling ? "colored" : "transparent"}`}>
       <img className="logo-navbar" src={logo} alt="" />
 
-      <ul className="nav-item-container">
+      <ul className={`nav-item-container ${hamburgerOpen ? "show" : "hidden"}`}>
         {navItems.map((navItem, index) => {
           return (
-            <li key={index} className="nav-item">
-              {navItem.name}
-            </li>
+            <a href="#">
+              <li key={index} className="nav-item">
+                {navItem.name}
+              </li>
+            </a>
           );
         })}
       </ul>
+
+      <img
+        className="nav-ham"
+        src={hamburgerOpen ? close : hamburger}
+        alt="hamburger"
+        onClick={() => setHamburgerOpen(!hamburgerOpen)}
+      />
     </div>
   );
 }
